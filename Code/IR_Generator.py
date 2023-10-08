65d67431d663cf51245a9a8b93292fff7c46b8a9
Evan Archer <evana@MacBook-Pro.lan>
2023-10-07 23:57:24 -0700

    added function within IR generator class to remove the silent sections from IR's to increase efficiency

--- Code/IR_Generator.py
+++ Code/IR_Generator.py
@@ -29,6 +29,17 @@ class IR_Generator:
         if len(initial_data.shape) > 1 and initial_data.shape[1] == 2:  # Check if impulse is stereo
                     initial_data = np.mean(initial_data, axis=1)
         normalized_initial_impulse = initial_data / np.max(np.abs(initial_data))
+        # section automatically removes silence from IR data to increase efficiency
+        noise_threshold = 10**(-3) #change this to edit IR and how much is cut
+        above_threshold_indices = np.where(abs(normalized_initial_impulse) > noise_threshold)[0]
+        if len(above_threshold_indices) > 0: #grabs the important parts of the IR file
+            IR_start_index = above_threshold_indices[0]
+            IR_end_index = above_threshold_indices[-1]
+        else:
+            IR_start_index = 0
+            IR_end_index = 0
+        #Extract and concatenate the non_silent sections
+        adjusted_normalized_initial_impulse = normalized_initial_impulse[IR_start_index:IR_end_index+1]
         try:
             for IR in self.inputs[1:]: #loop through second entry to last
                 ir_data, ir_rate = sf.read(IR)
@@ -36,35 +47,43 @@ class IR_Generator:
                     ir_data = np.mean(ir_data, axis=1)
                 normalized_impulse = ir_data / np.max(np.abs(ir_data))
                 
+                above_threshold_indices = np.where(abs(normalized_impulse) > noise_threshold)[0]
+                if len(above_threshold_indices) > 0: #grabs the important parts of the IR file
+                    IR_start_index = above_threshold_indices[0]
+                    IR_end_index = above_threshold_indices[-1]
+                else:
+                    IR_start_index = 0
+                    IR_end_index = 0
+                #Extract and concatenate the non_silent sections
+                adjusted_normalized_impulse = normalized_impulse[IR_start_index:IR_end_index+1]
+                
                 # Resample IR based on our input IR sample rate
                 if initial_rate != ir_rate:
-                    num_samples = int(len(normalized_impulse) * initial_rate / ir_rate)
-                    ir_resampled = resample(normalized_impulse, num_samples)
+                    num_samples = int(len(adjusted_normalized_impulse) * initial_rate / ir_rate)
+                    ir_resampled = resample(adjusted_normalized_impulse, num_samples)
                 else:
-                    ir_resampled = normalized_impulse
+                    ir_resampled = adjusted_normalized_impulse
                     
                 #check which signal needs to be padded
                 if len(ir_resampled)<len(initial_data):
-                    desired_length = len(normalized_initial_impulse)
+                    desired_length = len(adjusted_normalized_initial_impulse)
                     padded_impulse = np.pad(ir_resampled,(0, desired_length - len(ir_resampled)))
                     ir_resampled=padded_impulse
                 if len(ir_resampled)>len(initial_data): #pad initial if its shorter signal
                     desired_length = len(ir_resampled)
-                    padded_impulse = np.pad(normalized_initial_impulse,(0, desired_length - len(normalized_initial_impulse)))
-                    normalized_initial_impulse=padded_impulse
+                    padded_impulse = np.pad(adjusted_normalized_initial_impulse,(0, desired_length - len(adjusted_normalized_initial_impulse)))
+                    adjusted_normalized_initial_impulse=padded_impulse
                      
                 #put IR's together by convolution
                 ir_resampled_fft = np.fft.fft(ir_resampled)
-                initial_data_fft = np.fft.fft(normalized_initial_impulse)
+                initial_data_fft = np.fft.fft(adjusted_normalized_initial_impulse)
                 combined_impulse_fft = ir_resampled_fft*initial_data_fft
                 combined_impulse = np.real(np.fft.ifft(combined_impulse_fft))
                 initial_data = combined_impulse #update IR to be combination of IR's
             if len(initial_data.shape) > 1 and initial_data.shape[1] == 2:  # Check if impulse is stereo
                 initial_data = np.mean(initial_data, axis=1)  
         except: # if there is only 1 IR
-            if len(initial_data.shape) > 1 and initial_data.shape[1] == 2:  # Check if impulse is stereo
-                initial_data = np.mean(initial_data, axis=1)
-            return initial_data, initial_rate
+            return adjusted_normalized_initial_impulse, initial_rate
         finally:
             return initial_data, initial_rate
         
